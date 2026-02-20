69f7aff24fbca4b3013f0239dd325c04120c48f7
Ismaeel Bashir <93952193+ismaeelbashir03@users.noreply.github.com>
2026-02-20 21:10:19 +0000

    chore: update expanse-daemon to 0.6.2

--- Formula/expanse-daemon.rb
+++ Formula/expanse-daemon.rb
@@ -4,28 +4,28 @@
 class ExpanseDaemon < Formula
   desc "Daemon component for Expanse AI-native workflow orchestration"
   homepage "https://expanse.org.uk"
-  version "0.6.1"
+  version "0.6.2"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://releases.expanse.org.uk/daemon/v0.6.1/expanse-daemon_darwin_arm64.tar.gz"
-      sha256 "84ccb3e9fe134d2243985fa4b4e4dcc68916a26c1551136ac795086c3ee55280"
+      url "https://releases.expanse.org.uk/daemon/v0.6.2/expanse-daemon_darwin_arm64.tar.gz"
+      sha256 "90e23f9d21bd884f9e307788fc15010a8dd571af9f7e8d052ab68b3f4a543c26"
     end
     on_intel do
-      url "https://releases.expanse.org.uk/daemon/v0.6.1/expanse-daemon_darwin_amd64.tar.gz"
-      sha256 "eea48f2cd06fb2e8b0e0fdfaf9f7840af65c3e1b5bc87ef44622a0465a305930"
+      url "https://releases.expanse.org.uk/daemon/v0.6.2/expanse-daemon_darwin_amd64.tar.gz"
+      sha256 "adeff58e6ddcc1f40ddc64ef0fff993e33ee8aba94819b8c85fba3cb428da210"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://releases.expanse.org.uk/daemon/v0.6.1/expanse-daemon_linux_arm64.tar.gz"
-      sha256 "f49ba3de2c91e411337a90b61ee6d44cff7871cc3e4f80127d35c57214776b34"
+      url "https://releases.expanse.org.uk/daemon/v0.6.2/expanse-daemon_linux_arm64.tar.gz"
+      sha256 "ec3fd2d6a2204aba34c35c326c2370e7982d5da7b8eafcd5a396c762aacec522"
     end
     on_intel do
-      url "https://releases.expanse.org.uk/daemon/v0.6.1/expanse-daemon_linux_amd64.tar.gz"
-      sha256 "27f0c9011ba7d39699972ae06b8c570ecc7fd28d12e0a1ac562cd6ca80f4154f"
+      url "https://releases.expanse.org.uk/daemon/v0.6.2/expanse-daemon_linux_amd64.tar.gz"
+      sha256 "1d59db93b29c9d073c98feaa802d220d1fca8914542a05958d8bdf9281ce95f9"
     end
   end
 
