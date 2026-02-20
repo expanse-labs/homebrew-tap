c0baa1eb691837e27903d62cba4a6c6e541df9ca
Ismaeel Bashir <93952193+ismaeelbashir03@users.noreply.github.com>
2026-02-20 21:10:17 +0000

    chore: update expanse to 0.6.1

--- Formula/expanse.rb
+++ Formula/expanse.rb
@@ -4,28 +4,28 @@
 class Expanse < Formula
   desc "AI-native workflow orchestration for HPC"
   homepage "https://expanse.org.uk"
-  version "0.6.0"
+  version "0.6.1"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://releases.expanse.org.uk/cli/v0.6.0/expanse_darwin_arm64.tar.gz"
-      sha256 "9f52b3885d2ccd210e048af4083b7d247abe8d61a75b6d30589b453ac062e748"
+      url "https://releases.expanse.org.uk/cli/v0.6.1/expanse_darwin_arm64.tar.gz"
+      sha256 "06e74fb158cb39ac694c18bcdb1020ed79aca1e74830be76466995d99eab7d37"
     end
     on_intel do
-      url "https://releases.expanse.org.uk/cli/v0.6.0/expanse_darwin_amd64.tar.gz"
-      sha256 "14bd5989bec86c0bfa2a26a6f5fcdfecf66dde712f0323c246a5faae3f798340"
+      url "https://releases.expanse.org.uk/cli/v0.6.1/expanse_darwin_amd64.tar.gz"
+      sha256 "be206d57eab46c9fcfa84c22f69afc934fe1fc2a7ba6343e48979e95f2a06d88"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://releases.expanse.org.uk/cli/v0.6.0/expanse_linux_arm64.tar.gz"
-      sha256 "1c8e8ca584d4ddc45359007aaf5e812b00313b92fbe0d165ec25845d06247753"
+      url "https://releases.expanse.org.uk/cli/v0.6.1/expanse_linux_arm64.tar.gz"
+      sha256 "8f459e381b0f4f56eb78a88799148b579518e1fbc072ba7af2358e8163b5797f"
     end
     on_intel do
-      url "https://releases.expanse.org.uk/cli/v0.6.0/expanse_linux_amd64.tar.gz"
-      sha256 "df8871cb738f98c66abf9fce115250a5e8d6d9fa2d64416f6786c5e274089c5d"
+      url "https://releases.expanse.org.uk/cli/v0.6.1/expanse_linux_amd64.tar.gz"
+      sha256 "904c9a595c50870673153c75651614dea05cd889fb91ed455e6b3ffee0b6ad87"
     end
   end
 
